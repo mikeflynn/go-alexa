4c44e436bbe064a6afeb783ff769cfa0e915ad89
Mike Flynn <mfflynn@gmail.com>
2016-04-05 20:38:19 -0700

    Rewrote helloworld example to be easier with new request type hooks.

--- skillserver/examples/helloworld.go
+++ skillserver/examples/helloworld.go
@@ -1,38 +1,21 @@
 package main
 
 import (
-	"fmt"
-	"github.com/mikeflynn/go-alexa/skillserver"
-	"net/http"
+	alexa "github.com/mikeflynn/go-alexa/skillserver"
 )
 
 var Applications = map[string]interface{}{
-	"/echo/helloworld": skillserver.EchoApplication{ // Route
-		AppID:   "xxxxxxxx",     // Echo App ID from Amazon Dashboard
-		Handler: EchoHelloWorld, // Handler Func
-	},
-	"/": skillserver.StdApplication{
-		Methods: "GET",
-		Handler: HomePage,
+	"/echo/helloworld": alexa.EchoApplication{ // Route
+		AppID:    "xxxxxxxx", // Echo App ID from Amazon Dashboard
+		OnIntent: EchoIntentHandler,
+		OnLaunch: EchoIntentHandler,
 	},
 }
 
 func main() {
-	skillserver.Run(Applications, "3000")
+	alexa.Run(Applications, "3000")
 }
 
-func EchoHelloWorld(w http.ResponseWriter, r *http.Request) {
-	echoReq := skillserver.GetEchoRequest(r)
-
-	if echoReq.GetRequestType() == "IntentRequest" || echoReq.GetRequestType() == "LaunchRequest" {
-		echoResp := skillserver.NewEchoResponse().OutputSpeech("Hello world from my new Echo test app!").Card("Hello World", "This is a test card.")
-
-		json, _ := echoResp.String()
-		w.Header().Set("Content-Type", "application/json;charset=UTF-8")
-		w.Write(json)
-	}
-}
-
-func HomePage(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Home Page!")
+func EchoIntentHandler(echoReq *alexa.EchoRequest, echoResp *alexa.EchoResponse) {
+	echoResp.OutputSpeech("Hello world from my new Echo test app!").Card("Hello World", "This is a test card.")
 }
