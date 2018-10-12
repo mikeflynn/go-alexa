3e7fe53475f6d7117bb10758e5ca98059477f6ce
Rob King <rking188@gmail.com>
2018-10-12 17:45:18 -0400

    Golint improvements.

--- skillserver/echo.go
+++ skillserver/echo.go
@@ -3,21 +3,31 @@ package skillserver
 import (
 	"encoding/json"
 	"errors"
-	"github.com/mikeflynn/go-alexa/skillserver/dialog"
 	"time"
+
+	"github.com/mikeflynn/go-alexa/skillserver/dialog"
 )
 
+// ConfirmationStatus represents the status of either a dialog or slot confirmation.
 type ConfirmationStatus string
 
 const (
-	CONF_CONFIRMED ConfirmationStatus = "CONFIRMED"
-	CONF_DENIED    ConfirmationStatus = "DENIED"
-	CONF_NONE      ConfirmationStatus = "NONE"
+	// ConfConfirmed indicates the intent or slot has been confirmed by the end user.
+	ConfConfirmed ConfirmationStatus = "CONFIRMED"
+
+	// ConfDenied means the end user indicated the intent or slot should NOT proceed.
+	ConfDenied ConfirmationStatus = "DENIED"
+
+	// ConfNone means there has been not acceptance or denial of the intent or slot.
+	ConfNone ConfirmationStatus = "NONE"
 )
 
 // Request Functions
-func (this *EchoRequest) VerifyTimestamp() bool {
-	reqTimestamp, _ := time.Parse("2006-01-02T15:04:05Z", this.Request.Timestamp)
+
+// VerifyTimestamp will parse the timestamp in the EchoRequest and verify that it is in the correct
+// format and is not too old. True will be returned if the timestamp is valid; false otherwise.
+func (r *EchoRequest) VerifyTimestamp() bool {
+	reqTimestamp, _ := time.Parse("2006-01-02T15:04:05Z", r.Request.Timestamp)
 	if time.Since(reqTimestamp) < time.Duration(150)*time.Second {
 		return true
 	}
@@ -25,37 +35,45 @@ func (this *EchoRequest) VerifyTimestamp() bool {
 	return false
 }
 
-func (this *EchoRequest) VerifyAppID(myAppID string) bool {
-	if this.Session.Application.ApplicationID == myAppID ||
-		this.Context.System.Application.ApplicationID == myAppID {
+// VerifyAppID check that the incoming application ID matches the application ID provided
+// when running the server. This is a step required for skill certification.
+func (r *EchoRequest) VerifyAppID(myAppID string) bool {
+	if r.Session.Application.ApplicationID == myAppID ||
+		r.Context.System.Application.ApplicationID == myAppID {
 		return true
 	}
 
 	return false
 }
 
-func (this *EchoRequest) GetSessionID() string {
-	return this.Session.SessionID
+// GetSessionID is a convenience method for getting the session ID out of an EchoRequest.
+func (r *EchoRequest) GetSessionID() string {
+	return r.Session.SessionID
 }
 
-func (this *EchoRequest) GetUserID() string {
-	return this.Session.User.UserID
+// GetUserID is a convenience method for getting the user identifier out of an EchoRequest.
+func (r *EchoRequest) GetUserID() string {
+	return r.Session.User.UserID
 }
 
-func (this *EchoRequest) GetRequestType() string {
-	return this.Request.Type
+// GetRequestType is a convenience method for getting the request type out of an EchoRequest.
+func (r *EchoRequest) GetRequestType() string {
+	return r.Request.Type
 }
 
-func (this *EchoRequest) GetIntentName() string {
-	if this.GetRequestType() == "IntentRequest" {
-		return this.Request.Intent.Name
+// GetIntentName is a convenience method for getting the intent name out of an EchoRequest.
+func (r *EchoRequest) GetIntentName() string {
+	if r.GetRequestType() == "IntentRequest" {
+		return r.Request.Intent.Name
 	}
 
-	return this.GetRequestType()
+	return r.GetRequestType()
 }
 
-func (this *EchoRequest) GetSlotValue(slotName string) (string, error) {
-	slot, err := this.GetSlot(slotName)
+// GetSlotValue is a convenience method for getting the value of the specified slot out of an EchoRequest
+// as a string. An error is returned if a slot with that value is not found in the request.
+func (r *EchoRequest) GetSlotValue(slotName string) (string, error) {
+	slot, err := r.GetSlot(slotName)
 
 	if err != nil {
 		return "", err
@@ -64,24 +82,30 @@ func (this *EchoRequest) GetSlotValue(slotName string) (string, error) {
 	return slot.Value, nil
 }
 
-func (this *EchoRequest) GetSlot(slotName string) (EchoSlot, error) {
-	if _, ok := this.Request.Intent.Slots[slotName]; ok {
-		return this.Request.Intent.Slots[slotName], nil
+// GetSlot will return an EchoSlot from the EchoRequest with the given name.
+func (r *EchoRequest) GetSlot(slotName string) (EchoSlot, error) {
+	if _, ok := r.Request.Intent.Slots[slotName]; ok {
+		return r.Request.Intent.Slots[slotName], nil
 	}
 
-	return EchoSlot{}, errors.New("Slot name not found.")
+	return EchoSlot{}, errors.New("slot name not found")
 }
 
-func (this *EchoRequest) AllSlots() map[string]EchoSlot {
-	return this.Request.Intent.Slots
+// AllSlots will return a map of all the slots in the EchoRequest mapped by their name.
+func (r *EchoRequest) AllSlots() map[string]EchoSlot {
+	return r.Request.Intent.Slots
 }
 
 // Locale returns the locale specified in the request.
-func (this *EchoRequest) Locale() string {
-	return this.Request.Locale
+func (r *EchoRequest) Locale() string {
+	return r.Request.Locale
 }
 
 // Response Functions
+
+// NewEchoResponse will construct a new response instance with the required metadata and an empty speech string.
+// By default the response will indicate that the session should be ended. Use the `EndSession(bool)` method if the
+// session should be left open.
 func NewEchoResponse() *EchoResponse {
 	er := &EchoResponse{
 		Version: "1.0",
@@ -94,118 +118,136 @@ func NewEchoResponse() *EchoResponse {
 	return er
 }
 
-func (this *EchoResponse) OutputSpeech(text string) *EchoResponse {
-	this.Response.OutputSpeech = &EchoRespPayload{
+// OutputSpeech will replace any existing text that should be spoken with this new value. If the output
+// needs to be constructed in steps or special speech tags need to be used, see the `SSMLTextBuilder`.
+func (r *EchoResponse) OutputSpeech(text string) *EchoResponse {
+	r.Response.OutputSpeech = &EchoRespPayload{
 		Type: "PlainText",
 		Text: text,
 	}
 
-	return this
+	return r
 }
 
-func (this *EchoResponse) Card(title string, content string) *EchoResponse {
-	return this.SimpleCard(title, content)
+// Card will add a card to the Alexa app's response with the provided title and content strings.
+func (r *EchoResponse) Card(title string, content string) *EchoResponse {
+	return r.SimpleCard(title, content)
 }
 
-func (this *EchoResponse) OutputSpeechSSML(text string) *EchoResponse {
-	this.Response.OutputSpeech = &EchoRespPayload{
+// OutputSpeechSSML will add the text string provided and indicate the speech type is SSML in the response.
+// This should only be used if the text to speech string includes special SSML tags.
+func (r *EchoResponse) OutputSpeechSSML(text string) *EchoResponse {
+	r.Response.OutputSpeech = &EchoRespPayload{
 		Type: "SSML",
 		SSML: text,
 	}
 
-	return this
+	return r
 }
 
-func (this *EchoResponse) SimpleCard(title string, content string) *EchoResponse {
-	this.Response.Card = &EchoRespPayload{
+// SimpleCard will indicate that a card should be included in the Alexa companion app as part of the response.
+// The card will be shown with the provided title and content.
+func (r *EchoResponse) SimpleCard(title string, content string) *EchoResponse {
+	r.Response.Card = &EchoRespPayload{
 		Type:    "Simple",
 		Title:   title,
 		Content: content,
 	}
 
-	return this
+	return r
 }
 
-func (this *EchoResponse) StandardCard(title string, content string, smallImg string, largeImg string) *EchoResponse {
-	this.Response.Card = &EchoRespPayload{
+// StandardCard will indicate that a card should be shown in the Alexa companion app as part of the response.
+// The card shown will include the provided title and content as well as images loaded from the locations provided
+// as remote locations.
+func (r *EchoResponse) StandardCard(title string, content string, smallImg string, largeImg string) *EchoResponse {
+	r.Response.Card = &EchoRespPayload{
 		Type:    "Standard",
 		Title:   title,
 		Content: content,
 	}
 
 	if smallImg != "" {
-		this.Response.Card.Image.SmallImageURL = smallImg
+		r.Response.Card.Image.SmallImageURL = smallImg
 	}
 
 	if largeImg != "" {
-		this.Response.Card.Image.LargeImageURL = largeImg
+		r.Response.Card.Image.LargeImageURL = largeImg
 	}
 
-	return this
+	return r
 }
 
-func (this *EchoResponse) LinkAccountCard() *EchoResponse {
-	this.Response.Card = &EchoRespPayload{
+// LinkAccountCard is used to indicate that account linking still needs to be completed to continue
+// using the Alexa skill. This will force an account linking card to be shown in the user's companion app.
+func (r *EchoResponse) LinkAccountCard() *EchoResponse {
+	r.Response.Card = &EchoRespPayload{
 		Type: "LinkAccount",
 	}
 
-	return this
+	return r
 }
 
-func (this *EchoResponse) Reprompt(text string) *EchoResponse {
-	this.Response.Reprompt = &EchoReprompt{
+// Reprompt will send a prompt back to the user, this could be used to request additional information from the user.
+func (r *EchoResponse) Reprompt(text string) *EchoResponse {
+	r.Response.Reprompt = &EchoReprompt{
 		OutputSpeech: EchoRespPayload{
 			Type: "PlainText",
 			Text: text,
 		},
 	}
 
-	return this
+	return r
 }
 
-func (this *EchoResponse) RepromptSSML(text string) *EchoResponse {
-	this.Response.Reprompt = &EchoReprompt{
+// RepromptSSML is similar to the `Reprompt` method but should be used when the prompt
+// to the user should include special speech tags.
+func (r *EchoResponse) RepromptSSML(text string) *EchoResponse {
+	r.Response.Reprompt = &EchoReprompt{
 		OutputSpeech: EchoRespPayload{
 			Type: "SSML",
 			Text: text,
 		},
 	}
 
-	return this
+	return r
 }
 
-func (this *EchoResponse) EndSession(flag bool) *EchoResponse {
-	this.Response.ShouldEndSession = flag
+// EndSession is a convenience method for setting the flag in the response that will
+// indicate if the session between the end user's device and the skillserver should be closed.
+func (r *EchoResponse) EndSession(flag bool) *EchoResponse {
+	r.Response.ShouldEndSession = flag
 
-	return this
+	return r
 }
 
-// Delegate/Elicit/Confirm a dialog or an entire intent with user of alexa.
-// The func takes in name of the dialog, updated intent/intent to confirm if any and optional slot value.
-// It prepares a Echo Response to be returned.
-// Multiple directives can be returned by calling the method in chain (eg. RespondToIntent(...).RespondToIntent(...)
-//, each RespondToIntent call appends the data to Directives array and will return the same at the end.
-func (this *EchoResponse) RespondToIntent(name dialog.Type, intent *EchoIntent, slot *EchoSlot) *EchoResponse {
+// RespondToIntent is used to Delegate/Elicit/Confirm a dialog or an entire intent with
+// user of alexa. The func takes in name of the dialog, updated intent/intent to confirm
+// if any and optional slot value. It prepares a Echo Response to be returned.
+// Multiple directives can be returned by calling the method in chain
+// (eg. RespondToIntent(...).RespondToIntent(...), each RespondToIntent call appends the
+// data to Directives array and will return the same at the end.
+func (r *EchoResponse) RespondToIntent(name dialog.Type, intent *EchoIntent, slot *EchoSlot) *EchoResponse {
 	directive := EchoDirective{Type: name}
-	if intent != nil && name == dialog.CONFIRM_INTENT {
+	if intent != nil && name == dialog.ConfirmIntent {
 		directive.IntentToConfirm = intent.Name
 	} else {
 		directive.UpdatedIntent = intent
 	}
 
 	if slot != nil {
-		if name == dialog.ELICIT_SLOT {
+		if name == dialog.ElicitSlot {
 			directive.SlotToElicit = slot.Name
-		} else if name == dialog.CONFIRM_SLOT {
+		} else if name == dialog.ConfirmSlot {
 			directive.SlotToConfirm = slot.Name
 		}
 	}
-	this.Response.Directives = append(this.Response.Directives, &directive)
-	return this
+	r.Response.Directives = append(r.Response.Directives, &directive)
+	return r
 }
 
-func (this *EchoResponse) String() ([]byte, error) {
-	jsonStr, err := json.Marshal(this)
+func (r *EchoResponse) String() ([]byte, error) {
+	jsonStr, err := json.Marshal(r)
 	if err != nil {
 		return nil, err
 	}
@@ -215,6 +257,8 @@ func (this *EchoResponse) String() ([]byte, error) {
 
 // Request Types
 
+// EchoRequest represents all fields sent from the Alexa service to the skillserver.
+// Convenience methods are provided to pull commonly used properties out of the request.
 type EchoRequest struct {
 	Version string      `json:"version"`
 	Session EchoSession `json:"session"`
@@ -222,6 +266,8 @@ type EchoRequest struct {
 	Context EchoContext `json:"context"`
 }
 
+// EchoSession contains information about the ongoing session between the Alexa server and
+// the skillserver. This session is stored as part of each request.
 type EchoSession struct {
 	New         bool   `json:"new"`
 	SessionID   string `json:"sessionId"`
@@ -235,10 +281,12 @@ type EchoSession struct {
 	} `json:"user"`
 }
 
+// EchoContext contains information about the context in which the request was sent.
+// This could be information about the device from which the request was sent or about the invoked Alexa application.
 type EchoContext struct {
 	System struct {
 		Device struct {
-			DeviceId string `json:"deviceId,omitempty"`
+			DeviceID string `json:"deviceId,omitempty"`
 		} `json:"device,omitempty"`
 		Application struct {
 			ApplicationID string `json:"applicationId,omitempty"`
@@ -246,6 +294,7 @@ type EchoContext struct {
 	} `json:"System,omitempty"`
 }
 
+// EchoReqBody contains all data related to the type of request sent.
 type EchoReqBody struct {
 	Type        string     `json:"type"`
 	RequestID   string     `json:"requestId"`
@@ -256,12 +305,17 @@ type EchoReqBody struct {
 	DialogState string     `json:"dialogState,omitempty"`
 }
 
+// EchoIntent represents the intent that is sent as part of an EchoRequest. This includes
+// the name of the intent configured in the Alexa developers dashboard as well as any slots
+// and the optional confirmation status if one is needed to complete an intent.
 type EchoIntent struct {
 	Name               string              `json:"name"`
 	Slots              map[string]EchoSlot `json:"slots"`
 	ConfirmationStatus ConfirmationStatus  `json:"confirmationStatus"`
 }
 
+// EchoSlot represents variable values that can be sent that were specified by the end user
+// when invoking the Alexa application.
 type EchoSlot struct {
 	Name               string             `json:"name"`
 	Value              string             `json:"value"`
@@ -269,10 +323,17 @@ type EchoSlot struct {
 	ConfirmationStatus ConfirmationStatus `json:"confirmationStatus"`
 }
 
+// EchoResolution contains the results of entity resolutions when it relates to slots and how
+// the values are resolved. The resolutions will be organized by authority, for custom slots
+// the authority will be the custom slot type that was defined.
+// Find more information here: https://developer.amazon.com/docs/custom-skills/define-synonyms-and-ids-for-slot-type-values-entity-resolution.html#intentrequest-changes
 type EchoResolution struct {
 	ResolutionsPerAuthority []EchoResolutionPerAuthority `json:"resolutionsPerAuthority"`
 }
 
+// EchoResolutionPerAuthority contains information about a single slot resolution from a single
+// authority. The values silce will contain all possible matches for different slots.
+// These resolutions are most interesting when working with synonyms.
 type EchoResolutionPerAuthority struct {
 	Authority string `json:"authority"`
 	Status    struct {
@@ -286,12 +347,17 @@ type EchoResolutionPerAuthority struct {
 
 // Response Types
 
+// EchoResponse represents the information that should be sent back to the Alexa service
+// from the skillserver.
 type EchoResponse struct {
 	Version           string                 `json:"version"`
 	SessionAttributes map[string]interface{} `json:"sessionAttributes,omitempty"`
 	Response          EchoRespBody           `json:"response"`
 }
 
+// EchoRespBody contains the body of the response to be sent back to the Alexa service.
+// This includes things like the text that should be spoken or any cards that should
+// be shown in the Alexa companion app.
 type EchoRespBody struct {
 	OutputSpeech     *EchoRespPayload `json:"outputSpeech,omitempty"`
 	Card             *EchoRespPayload `json:"card,omitempty"`
@@ -300,15 +366,21 @@ type EchoRespBody struct {
 	Directives       []*EchoDirective `json:"directives,omitempty"`
 }
 
+// EchoReprompt contains speech that should be spoken back to the end user to retrieve
+// additional information or to confirm an action.
 type EchoReprompt struct {
 	OutputSpeech EchoRespPayload `json:"outputSpeech,omitempty"`
 }
 
+// EchoRespImage represents a single image with two variants that should be returned as part
+// of a response. Small and Large image sizes can be provided.
 type EchoRespImage struct {
 	SmallImageURL string `json:"smallImageUrl,omitempty"`
 	LargeImageURL string `json:"largeImageUrl,omitempty"`
 }
 
+// EchoRespPayload contains the interesting parts of the Echo response including text to be spoken,
+// card attributes, and images.
 type EchoRespPayload struct {
 	Type    string        `json:"type,omitempty"`
 	Title   string        `json:"title,omitempty"`
@@ -318,6 +390,9 @@ type EchoRespPayload struct {
 	Image   EchoRespImage `json:"image,omitempty"`
 }
 
+// EchoDirective includes information about intents and slots that should be confirmed or elicted from the user.
+// The type value can be used to delegate the action to the Alexa service. In this case, a pre-configured prompt
+// will be used from the developer console.
 type EchoDirective struct {
 	Type            dialog.Type `json:"type"`
 	UpdatedIntent   *EchoIntent `json:"updatedIntent,omitempty"`
